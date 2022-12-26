7d0ac84f8524e7695734803185e02da0d7b30ff8
Blackmage <fazadoang15@gmail.com>
2022-12-26 16:50:52 +0700

    feat: add basic sort

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("../test-files/numbers.txt").unwrap();
+    let reader = BufReader::new(file);
+    let mut values: Vec<i32> = Vec::new();
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let value: i32 = line.parse().unwrap();
+        values.push(value);
+    }
+
+    values.sort();
+
+    println!("{:?}", values);
 }
