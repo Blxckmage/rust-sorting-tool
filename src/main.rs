60c98ef6747b15f4d22685a9ea45ca4c57fc94fc
Blackmage <fazadoang15@gmail.com>
2022-12-26 17:35:06 +0700

    feat: add heap sort for faster sort and show elapsed time

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::time::Instant;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,12 +17,54 @@ fn main() {
         values.push(value);
     }
 
-    values.sort();
+    let start_time = Instant::now();
 
-    let mut output_file = File::create(output_file).unwrap();
+    merge_sort(&mut values);
+
+    let elapsed_time = start_time.elapsed();
+    println!("Elapsed time: {:?}", elapsed_time);
+
+    let output_file = File::create(output_file).unwrap();
     let mut writer = BufWriter::new(output_file);
 
     for value in values {
-        writeln!(writer, "{} ", value).unwrap();
+        writeln!(writer, "{}", value).unwrap();
+    }
+}
+
+fn merge_sort(values: &mut [i32]) {
+    if values.len() > 1 {
+        let mid = values.len() / 2;
+        let mut left = values[..mid].to_vec();
+        let mut right = values[mid..].to_vec();
+        merge_sort(&mut left);
+        merge_sort(&mut right);
+        merge(values, &left, &right);
+    }
+}
+
+fn merge(values: &mut [i32], left: &[i32], right: &[i32]) {
+    let mut i = 0;
+    let mut j = 0;
+    let mut k = 0;
+    while i < left.len() && j < right.len() {
+        if left[i] < right[j] {
+            values[k] = left[i];
+            i += 1;
+        } else {
+            values[k] = right[j];
+            j += 1;
+        }
+        k += 1;
+    }
+    while i < left.len() {
+        values[k] = left[i];
+        i += 1;
+        k += 1;
+    }
+    while j < right.len() {
+        values[k] = right[j];
+        j += 1;
+        k += 1;
     }
 }
